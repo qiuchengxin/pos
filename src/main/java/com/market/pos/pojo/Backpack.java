6c30216f480d5810ab23448245235cd798529ded
Administrator <18031962065@163.com>
2018-12-04 18:53:41 +0800

    新建BackPack处理，测试，可回滚

---
+++ src/main/java/com/market/pos/pojo/Backpack.java
@@ -0,0 +1,63 @@
+package com.market.pos.pojo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+public class Backpack {
+
+  private int userid;
+  private String equipment;
+  private String money;
+  private String book;
+  private String special;
+  private int finddaily;
+
+  public int getUserid() {
+    return userid;
+  }
+
+  public void setUserid(int userid) {
+    this.userid = userid;
+  }
+
+  public String getEquipment() {
+    return equipment;
+  }
+
+  public void setEquipment(String equipment) {
+    this.equipment = equipment;
+  }
+
+  public String getMoney() {
+    return money;
+  }
+
+  public void setMoney(String money) {
+    this.money = money;
+  }
+
+  public String getBook() {
+    return book;
+  }
+
+  public void setBook(String book) {
+    this.book = book;
+  }
+
+  public String getSpecial() {
+    return special;
+  }
+
+  public void setSpecial(String special) {
+    this.special = special;
+  }
+
+  public int getFinddaily() {
+    return finddaily;
+  }
+
+  public void setFinddaily(int finddaily) {
+    this.finddaily = finddaily;
+  }
+}
