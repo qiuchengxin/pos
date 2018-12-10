f1e845e69124a50f2f2a2fc3ec9f956cf84962af
qiuchengxin <18031962065@163.com>
2018-12-10 17:30:48 +0800

    ### 12/10 切分数据库，将QQMsg中提取出groupid作为数据库的名称 新增：排名系统Rank，优化闭关操作

---
+++ src/main/java/com/market/pos/tool/Rank/JdbcRank.java
@@ -0,0 +1,77 @@
+package com.market.pos.tool.Rank;
+
+import java.sql.*;
+
+public class JdbcRank {
+
+    static Connection connection;
+    static String driver = "com.mysql.cj.jdbc.Driver";
+    static String user = "root";
+    static String password = "123456";
+
+    public static String first_username;
+    public static String second_username;
+    public static String third_username;
+    public static int first_grade;
+    public static int second_grade;
+    public static int third_grade;
+
+    public static int myRankNum;
+
+    public static void searchRank(String sql,String groupid){
+        String url = "jdbc:mysql://localhost:3306/" + groupid + "?useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8";
+        try{
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url,user,password);
+            if(!connection.isClosed()){
+                System.out.println("----------------连接成功---------------");
+            }
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            while (result.next()){
+                String username = result.getString("username");
+                int grade = result.getInt("grade");
+                int rowNo = result.getRow();
+                if (rowNo == 1){
+                    first_username = username;
+                    first_grade = grade;
+                }
+                if (rowNo == 2){
+                    second_username = username;
+                    second_grade = grade;
+                }
+                if (rowNo == 3){
+                    third_username = username;
+                    third_grade = grade;
+                }
+            }
+            connection.close();
+        }catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void searchRankByUserid(String sql,String groupid){
+        String url = "jdbc:mysql://localhost:3306/" + groupid + "?useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8";
+        myRankNum = 0;
+        try{
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url,user,password);
+            if(!connection.isClosed()){
+                System.out.println("----------------连接成功---------------");
+            }
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            while (result.next()){
+                myRankNum = result.getInt("rowNo");
+            }
+            connection.close();
+        }catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/market/pos/tool/Rank/RankService.java
@@ -0,0 +1,39 @@
+package com.market.pos.tool.Rank;
+
+public class RankService {
+    public static String first_username;
+    public static String second_username;
+    public static String third_username;
+    public static int first_grade;
+    public static int second_grade;
+    public static int third_grade;
+
+    public static int myRowNo;
+
+    /**
+     * 查询排行前三的信息
+     */
+    public static void selectAllRank(String groupid){
+        String sql = "select username, grade from users order by grade desc LIMIT 3";
+        JdbcRank.searchRank(sql,groupid);
+        first_username = JdbcRank.first_username;
+        first_grade = JdbcRank.first_grade;
+        second_username = JdbcRank.second_username;
+        second_grade = JdbcRank.second_grade;
+        third_username = JdbcRank.third_username;
+        third_grade = JdbcRank.third_grade;
+    }
+
+    /**
+     * 查询指定userid用户的排名
+     * @param userid
+     */
+    public static void selectRankByUserid(String userid,String groupid){
+        String sql = "select users.rowNo from " +
+                "(select userid, (@rowNum := @rowNum + 1) as rowNo from " +
+                "users , (select (@rowNum := 0)) b order by grade desc) " +
+                "users where users.userid = " + "'" + userid + "'";
+        JdbcRank.searchRankByUserid(sql,groupid);
+        myRowNo = JdbcRank.myRankNum;
+    }
+}
