b508bbe6e072d460a8875d75f76213598131fc51
qcx work <1>
2019-03-22 15:47:15 +0800

    03/12 Rank模块bug修改

--- src/main/java/com/market/pos/tool/Rank/JdbcRank.java
+++ src/main/java/com/market/pos/tool/Rank/JdbcRank.java
@@ -1,13 +1,16 @@
 package com.market.pos.tool.Rank;
 
+import com.market.pos.config.DBClose;
+import com.market.pos.tool.common.FinalData;
+
 import java.sql.*;
 
 public class JdbcRank {
 
-    static Connection connection;
+    static String jdbcurl = FinalData.JDBCURL;
     static String driver = "com.mysql.cj.jdbc.Driver";
-    static String user = "root";
-    static String password = "123456";
+    static String user = FinalData.SQLUSRERNAME;
+    static String password = FinalData.SQLPASSWORD;
 
     public static String first_username;
     public static String second_username;
@@ -19,19 +22,22 @@ public class JdbcRank {
     public static int myRankNum;
 
     public static void searchRank(String sql,String groupid){
-        String url = "jdbc:mysql://148.70.49.2:3306/" + groupid + "?useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8";
+        String url = jdbcurl + groupid + "?useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8";
+        ResultSet resultSet = null;
+        Connection connection = null;
+        Statement statement = null;
         try{
             Class.forName(driver);
             connection = DriverManager.getConnection(url,user,password);
             if(!connection.isClosed()){
                 System.out.println("----------------连接成功---------------");
             }
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery(sql);
-            while (result.next()){
-                String username = result.getString("username");
-                int grade = result.getInt("grade");
-                int rowNo = result.getRow();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+            while (resultSet.next()){
+                String username = resultSet.getString("username");
+                int grade = resultSet.getInt("grade");
+                int rowNo = resultSet.getRow();
                 if (rowNo == 1){
                     first_username = username;
                     first_grade = grade;
@@ -45,33 +51,38 @@ public class JdbcRank {
                     third_grade = grade;
                 }
             }
-            connection.close();
         }catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            DBClose.colseSqlConnection(resultSet,statement,connection);
         }
     }
 
     public static void searchRankByUserid(String sql,String groupid){
         String url = "jdbc:mysql://148.70.49.2:3306/" + groupid + "?useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8";
         myRankNum = 0;
+        ResultSet result = null;
+        Connection connection = null;
+        Statement statement = null;
         try{
             Class.forName(driver);
             connection = DriverManager.getConnection(url,user,password);
             if(!connection.isClosed()){
                 System.out.println("----------------连接成功---------------");
             }
-            Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery(sql);
+            statement = connection.createStatement();
+            result = statement.executeQuery(sql);
             while (result.next()){
                 myRankNum = result.getInt("rowNo");
             }
-            connection.close();
         }catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
+        }finally {
+            DBClose.colseSqlConnection(result,statement,connection);
         }
     }
 }
